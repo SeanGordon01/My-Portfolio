04965e26af2a037a90ce66216b4ddc00c1446395
Sean Gordon <seangordon@Seans-iMac.local>
2023-04-21 15:40:23 +0100

    Portfolio Website Finished

---
+++ src/app.js
@@ -0,0 +1,36 @@
+// const footerIcons = document.querySelectorAll('.footer-icon');
+
+// footerIcons.forEach(icon => {
+//   icon.style.filter = 'invert(1)';
+//   icon.addEventListener('mouseover', () => {
+//   });
+// });
+
+// get the image elements
+// const emailIcon = document.querySelector(".footer-icon[src='src/images/email-icon.svg']");
+// const linkedinIcon = document.querySelector(".footer-icon[src='src/images/linkedin-icon.svg']");
+// const githubIcon = document.querySelector(".footer-icon[src='src/images/behance-icon.svg']");
+
+// // add event listeners to open links on click
+// emailIcon.addEventListener("click", function() {
+//   window.open("mailto:greghi.fumagalli@gmail.com");
+// });
+
+// linkedinIcon.addEventListener("click", function() {
+//   window.open("https://www.linkedin.com/in/gregorio-fumagalli/");
+// });
+
+// githubIcon.addEventListener("click", function() {
+//   window.open("https://github.com/Greghi2000");
+// });
+
+// const linkedinIconContact = document.querySelector(".contact-icon[src='src/images/linkedin-logo.svg']");
+// const githubIconContact = document.querySelector(".contact-icon[src='src/images/github-logo.svg']");
+
+// linkedinIconContact.addEventListener("click", function() {
+//   window.open("https://www.linkedin.com/in/gregorio-fumagalli/");
+// });
+
+// githubIconContact.addEventListener("click", function() {
+//   window.open("https://github.com/Greghi2000");
+// });
